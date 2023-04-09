18d519a8ab38d2b85a23e104822a4c7798dd00da
Jerome Rasky <jyrome.112@gmail.com>
2023-04-09 11:05:55 -0700

    Initial template

---
+++ test/kobold.house.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KoboldHouse from '../lib/kobold.house-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kobold.house-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KoboldHouse.KoboldHouseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
